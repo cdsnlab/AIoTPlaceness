975da327a06910013713163715e416f2a3ddc489
Dongmin Kim <underkoo@gmail.com>
2019-09-15 20:26:47 +0900

    bug fix

--- social-activity-extractor/_8_multimodal_processing.py
+++ social-activity-extractor/_8_multimodal_processing.py
@@ -17,6 +17,7 @@ def do_clustering(target_dataset, cluster_method):
 	df_data = pd.read_csv(os.path.join(CONFIG.CSV_PATH, 'ds_' + target_dataset + '.csv'), index_col=0, header=None, encoding='utf-8-sig')
 	df_data.index.name = 'short_code'
 	print(df_data.iloc[:100])
+	print(df_data.shape)
 
 	start_time = time.time()
 	if cluster_method == 0:
@@ -39,7 +40,7 @@ def do_clustering(target_dataset, cluster_method):
 	print(clustering.labels_)
 	count_percentage(clustering.labels_)
 	result_df = pd.DataFrame(data=clustering.labels_, index=df_data.index, columns=['cluster'])
-	result_df_data.to_csv(os.path.join(CONFIG.CSV_PATH, csv_name), encoding='utf-8-sig')
+	result_df.to_csv(os.path.join(CONFIG.CSV_PATH, csv_name), encoding='utf-8-sig')
 
 def count_percentage(cluster_labels):
 	count = collections.Counter(cluster_labels)
