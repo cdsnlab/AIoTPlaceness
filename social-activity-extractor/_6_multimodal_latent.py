0ac65f1cc5a600005dcc8c1c4318178cfe67bbcf
Dongmin Kim <underkoo@gmail.com>
2019-09-20 12:19:05 +0900

    latent update

--- social-activity-extractor/_6_multimodal_latent.py
+++ social-activity-extractor/_6_multimodal_latent.py
@@ -47,13 +47,18 @@ def main():
 	parser.add_argument('-batch_size', type=int, default=16, help='batch size for training')
 
 	# model
-	parser.add_argument('-latent_size', type=int, default=900, help='size of latent variable')
+	parser.add_argument('-latent_size', type=int, default=1000, help='size of latent variable')
 	parser.add_argument('-filter_size', type=int, default=300, help='filter size of convolution')
 	parser.add_argument('-filter_shape', type=int, default=5,
 						help='filter shape to use for convolution')
 	parser.add_argument('-arch', type=str, default='resnext101_32x8d', help='image embedding model')
 	parser.add_argument('-image_embedding_dim', type=int, default=2048, help='embedding dimension of the model')
 	parser.add_argument('-num_layer', type=int, default=4, help='layer number')
+	parser.add_argument('-encode_latent', type=int, default=1000, help='latent vector size of encoder')
+	parser.add_argument('-decode_latent', type=int, default=1000, help='latent vector size of decoder')
+	parser.add_argument('-normalize', action='store_true', default=False, help='normalize before fusion')
+	parser.add_argument('-add_latent', action='store_true', default=False, help='add latent or concat')
+	parser.add_argument('-no_decode', action='store_true', default=False, help='no decode')
 
 	# train
 	parser.add_argument('-noti', action='store_true', default=False, help='whether using gpu server')
@@ -91,18 +96,18 @@ def get_latent(args):
 	args.t3 = t3
 
 	text_embedding = nn.Embedding.from_pretrained(torch.FloatTensor(text_embedding_model))
-	text_encoder = text_model.ConvolutionEncoder(text_embedding, t3, args.filter_size, args.filter_shape, args.latent_size)
-	text_decoder = text_model.DeconvolutionDecoder(text_embedding, args.tau, t3, args.filter_size, args.filter_shape, args.latent_size, device)
-	imgseq_encoder = imgseq_model.RNNEncoder(args.image_embedding_dim, args.num_layer, args.latent_size, bidirectional=True)
-	imgseq_decoder = imgseq_model.RNNDecoder(CONFIG.MAX_SEQUENCE_LEN, args.image_embedding_dim, args.num_layer, args.latent_size, bidirectional=True)
-	multimodal_encoder = multimodal_model.MultimodalEncoder(text_encoder, imgseq_encoder, args.latent_size)
+	text_encoder = text_model.ConvolutionEncoder(text_embedding, t3, args.filter_size, args.filter_shape, args.encode_latent)
+	imgseq_encoder = imgseq_model.RNNEncoder(args.image_embedding_dim, args.num_layer, args.encode_latent, bidirectional=True)
+	multimodal_encoder = multimodal_model.MultimodalEncoder(text_encoder, imgseq_encoder, args.latent_size, args.normalize, args.add_latent)
 	checkpoint = torch.load(os.path.join(CONFIG.CHECKPOINT_PATH, args.checkpoint), map_location=lambda storage, loc: storage)
 	multimodal_encoder.load_state_dict(checkpoint['multimodal_encoder'])
 	multimodal_encoder.to(device)
 	multimodal_encoder.eval() 
 
-	f_csv = open(os.path.join(CONFIG.CSV_PATH, 'latent_' + args.target_dataset + '.csv'), 'w', encoding='utf-8-sig')
-	wr = csv.writer(f_csv)
+	#f_csv = open(os.path.join(CONFIG.CSV_PATH, 'latent_' + args.target_dataset + '.csv'), 'w', encoding='utf-8-sig')
+	#wr = csv.writer(f_csv)
+	short_code_list = []
+	row_list = []
 	for text_batch, imgseq_batch, short_code in tqdm(full_loader):
 		torch.cuda.empty_cache()
 		with torch.no_grad():	
@@ -111,10 +116,15 @@ def get_latent(args):
 		h = multimodal_encoder(text_feature, imgseq_feature)
 
 		for _short_code, _h in zip(short_code, h):
-			row = [_short_code] + _h.detach().cpu().numpy().tolist()
-			wr.writerow(row)
+			short_code_list.append(short_code)
+			row_list.append(_h.detach().cpu().numpy().tolist())
+			# row = [_short_code] + _h.detach().cpu().numpy().tolist()
+			# wr.writerow(row)
 		del text_feature, imgseq_feature
-	f_csv.close()
+	#f_csv.close()
+	result_df = pd.DataFrame(data=row_list, index=short_code_list, columns=[i for i in range(args.latent_size)])
+	result_df.sort_index(inplace=True)
+	result_df.to_csv(os.path.join(CONFIG.CSV_PATH, 'clustered_spectral_' + target_dataset + '.csv'), encoding='utf-8-sig')
 	print("Finish!!!")
 
 
