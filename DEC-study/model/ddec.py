49dcb1f42cb9bad1cb9ee83c5b08a30fc28d911d
Dongmin Kim <underkoo@gmail.com>
2020-04-12 04:24:10 +0900

    add text train or text

--- DEC-study/model/ddec.py
+++ DEC-study/model/ddec.py
@@ -119,7 +119,7 @@ class DualNet(nn.Module):
             train_acc = accuracy_score(train_labels, train_pred)
             train_nmi = normalized_mutual_info_score(train_labels, train_pred, average_method='geometric')
             train_f_1 = f1_score(train_labels, train_pred, average='macro', labels=np.unique(train_pred))
-            print("#Epoch %3d: acc: %.4f, nmi: %.4f, f_1: %.4f, loss: %.4f at %s" % (
+            print("#Train Epoch %3d: acc: %.4f, nmi: %.4f, f_1: %.4f, loss: %.4f at %s" % (
                 epoch + 1, train_acc, train_nmi, train_f_1, train_loss, str(datetime.datetime.now())))
 
             self.eval()
@@ -143,7 +143,7 @@ class DualNet(nn.Module):
             test_acc = accuracy_score(test_labels, test_pred)
             test_nmi = normalized_mutual_info_score(test_labels, test_pred, average_method='geometric')
             test_f_1 = f1_score(test_labels, test_pred, average='macro', labels=np.unique(test_pred))
-            print("#Epoch %3d: acc: %.4f, nmi: %.4f, f_1: %.4f, loss: %.4f at %s" % (
+            print("#Test Epoch %3d: acc: %.4f, nmi: %.4f, f_1: %.4f, loss: %.4f at %s" % (
                 epoch + 1, test_acc, test_nmi, test_f_1, test_loss, str(datetime.datetime.now())))
         if save_path:
             self.save_model(save_path)
