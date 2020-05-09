103833995c5e92f8369c8e8ff8d8b9ce898d4082
Dongmin Kim <underkoo@gmail.com>
2020-05-09 09:17:56 +0900

    bug fix

--- social-activity-extractor/model/multidec.py
+++ social-activity-extractor/model/multidec.py
@@ -324,8 +324,8 @@ class MultiDEC(nn.Module):
         initial_pred = torch.argmax(p, dim=1).numpy()
         df_initial = pd.DataFrame(data=initial_pred, index=full_short_codes + test_short_codes, columns=['label'])
         df_initial['pred'] = 'pred'
-        df_initial.loc[df_train.index]['pred'] = 'label'
-        df_initial.loc[df_test.index]['pred'] = 'label'
+        df_initial.loc[df_train.index, 'pred'] = 'label'
+        df_initial.loc[df_test.index, 'pred'] = 'label'
 
         print("Conducting TSNE at %s" % (str(datetime.datetime.now())))
         do_tsne(p.numpy(), df_initial, self.n_clusters, os.path.join(CONFIG.SVG_PATH, 'test.png'))
